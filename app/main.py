faa5780559638985295cc1bfc4b1b2c11798d1f4
R1ch3rd <richysamdom@gmail.com>
2024-08-29 22:34:07 +0530

    defined first schema

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.templating import Jinja2Templates
+import schemas
 
 app = FastAPI()
 templates = Jinja2Templates(directory = "templates")
@@ -10,3 +11,15 @@ templates = Jinja2Templates(directory = "templates")
 @app.get('/index', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html",{"request": request})
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers = ["*"],
+)
+
+@app.post("/translate",response_model=schemas.TaskResponse)
+def translate(request: schemas.TranslationRequest):
+    # 33:24
